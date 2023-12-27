adef52f8c9087bcf7cefc4a33adb8a8621497c47
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2023-12-27 19:16:58 +0530

    Update Easy-Anagram.js

--- Week-1 Assignments/01 JavaScript/Easy-Anagram.js
+++ Week-1 Assignments/01 JavaScript/Easy-Anagram.js
@@ -1,1 +1,15 @@
+ const isAnagram = function (firstString, secondString){
+ var newFirstString = firstString.toLowerCase().split('').sort().join('');
+ var newSecondString = secondString.toLowerCase().split('').sort().join('');
 
+  if(newFirstString === newSecondString){
+    console.log("String is Anagram");
+   }
+  else{
+    console.log("Not an Anagram");
+  }
+}
+var firstString = "hrhsa";
+var secondString = "harsh";
+
+isAnagram(firstString, secondString);
