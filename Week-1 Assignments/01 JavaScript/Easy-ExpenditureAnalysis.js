86192dc956d3c9f1e94ac2f52d2aaa92f9f3ba43
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2023-12-27 20:23:51 +0530

    Update Easy-ExpenditureAnalysis.js

--- Week-1 Assignments/01 JavaScript/Easy-ExpenditureAnalysis.js
+++ Week-1 Assignments/01 JavaScript/Easy-ExpenditureAnalysis.js
@@ -1,1 +1,30 @@
+const calculateTotalSpentByCategory = (transactions) => {
+  let totalPrice = 0;
+  let categoryFound = false;
+  let enteredCategory = "Food";
+
+  transactions.forEach((elem) => {
+    const { category, price } = elem;
+    if (category === enteredCategory) {
+      totalPrice = totalPrice + price;
+      categoryFound = true;
+    }
+  });
+
+  if (!categoryFound) {
+    console.log(enteredCategory,"Is Not Found");
+  } else {
+    console.log("Total price of", enteredCategory,"₹",totalPrice);
+  }
+};
+
+transactions = [
+  { category: "Drink", itemName: "Coke", price: 20, timestamp: "12-Jun-2022" },
+  { category: "Food", itemName: "Burger", price: 50, timestamp: "12-Jun-2022" },
+  { category: "Drink", itemName: "Fanta", price: 25, timestamp: "12-Jun-2022" },
+  { category: "Drink", itemName: "Sprite", price: 35, timestamp: "12-Jun-2022"},
+  { category: "Food", itemName: "Pasta", price: 75, timestamp: "12-Jun-2022" },
+];
+
+calculateTotalSpentByCategory(transactions);
 
