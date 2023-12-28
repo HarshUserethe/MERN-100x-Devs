caf9460fdfdf8d4016692c03dd25574cd7fb9136
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2023-12-28 10:52:17 +0530

    Update Easy-Counter.js

--- Week-1 Assignments/02 Async-JS/Easy-Counter.js
+++ Week-1 Assignments/02 Async-JS/Easy-Counter.js
@@ -1,1 +1,29 @@
+//01 Implemented Counter Using setTimeout();
+
+ const counter = (number) => {
+   console.log(number); //updated value after every setTimeout();
+   
+   if(number < 10){  
+     number++;
+
+     setTimeout(function(){
+       counter(number);
+     }, 1000);
+   }
+ }
+
+counter(1);
+
+
+
+//02 Implemented Counter Using setInterval();
+
+var number = 0;
+
+const counter2 = () => {
+setInterval(function(){
+  number = number + 1;
+  console.log(number);
+}, 1000)
+}
 
